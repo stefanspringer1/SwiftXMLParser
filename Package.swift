96513342f62cf220ee379e1df1306396e7ca8583
stefanspringer1 <stefan.springer@icloud.com>
2021-08-21 12:13:34 +0200

    use SwiftXMLInterfaces >= 0.1.20

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["SwiftXMLParser"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/stefanspringer1/SwiftXMLInterfaces", from: "0.1.19"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXMLInterfaces", from: "0.1.20"),
     ],
     targets: [
         .target(
