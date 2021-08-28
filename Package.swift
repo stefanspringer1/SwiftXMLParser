9b5a63ebaf809c7411f6d63060062670a8918861
stefanspringer1 <stefan.springer@icloud.com>
2021-08-28 16:26:37 +0200

    using SwiftXMLInterfaces >= 0.1.44; using swift-tools-version:5.3 again

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["SwiftXMLParser"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/stefanspringer1/SwiftXMLInterfaces.git", from: "0.1.43"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXMLInterfaces.git", from: "0.1.44"),
     ],
     targets: [
         .target(
